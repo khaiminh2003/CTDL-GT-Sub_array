f5085162e94412a737a74c4e22aa8b9c9fcd8b9e
khaiminh2003 <103360909+khaiminh2003@users.noreply.github.com>
2022-04-10 17:10:55 +0700

    Add files via upload

---
+++ baitap.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+int nhapMang(int a[], int n) {              
+	for(int i=0; i<n; i++) {
+		scanf("%d", &a[i]); 
+	} 
+} 
+
+
+int xuatMang(int a[], int n) {			
+	printf("Day a: ");
+	for(int i=0; i<n; i++) {
+		printf("%d  ", a[i]); 
+	} 
+} 
+
+
+void tongklonnhat(int a[],int n,int k) {          
+	int vitri=0,max=0,s,sum=0;
+	for(int j=0;j<=n-k;j++)
+	{
+		s=0;
+		for(int i=j;i<j+k;i++)
+		{
+			s=s+a[i];
+		}
+		if(s>max)
+		{
+			max=s;
+			vitri=j;
+		}
+	}
+	printf("\nDay con co khoi luong lon nhat khi k=%d la: ", k);
+	for(int q = vitri;q<vitri+k;q++)
+	{
+		printf("%d  ", a[q]);
+		sum=sum+a[q];		
+	}
+	printf("\nKhoi luong day con lon: %d", sum); 
+}
+
+
+
+int main() {
+	int n, a[n], k;
+	printf("Nhap vao do dai day a: ");
+	scanf("%d", &n);
+	printf("Nhap vao k:"); 
+	scanf("%d",&k);
+	nhapMang(a, n);
+    xuatMang(a, n); 
+	tongklonnhat(a, n, k);
+	return 0; 
+} 
